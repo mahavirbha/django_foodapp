ec700a606a7097a9715c988b050f3cbddfa65a8b
Mahavirbha <robowithsomeaix@gmail.com>
2022-09-12 22:44:27 +0530

    render using context

--- food/views.py
+++ food/views.py
@@ -1,19 +1,22 @@
 from re import template
+
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Item
 from django.template import loader
+
+from .models import Item
 
 # Create your views here.
 
 
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
+    # template = loader.get_template('food/index.html')
     context = {
-
+        'item_list':item_list,
     }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
