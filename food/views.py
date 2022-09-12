e9d28ce2a9a77377e7d0fc68fccc28ac7e6a4192
Mahavirbha <robowithsomeaix@gmail.com>
2022-09-12 09:39:20 +0530

    retrivieng data from database

--- food/views.py
+++ food/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse('Hello World')
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
+
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
