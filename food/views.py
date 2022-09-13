c3508aca3d58d7bb4bb04114004e757ed6ca037a
Mahavirbha <robowithsomeaix@gmail.com>
2022-09-13 09:19:58 +0530

    details view added

--- food/views.py
+++ food/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from re import template
 
 from django.http import HttpResponse
@@ -13,10 +14,19 @@ def index(request):
     item_list = Item.objects.all()
     # template = loader.get_template('food/index.html')
     context = {
-        'item_list':item_list,
+        'item_list': item_list,
     }
     # return HttpResponse(template.render(context, request))
     return render(request, 'food/index.html', context)
 
+
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk == item_id)
+    context = {
+        'item': item
+    }
+    return render(request,  'food/detail.html', context)
