f157dfd9b7d6926f495894d14926f8d8dfb656d3
Mahavirbha <robowithsomeaix@gmail.com>
2022-12-20 23:45:37 +0530

    Delete Button-Functionality added

--- food/urls.py
+++ food/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('add', views.create_item, name='create_item'),
     # update item
     path('update/<int:id>/', views.update_item, name='update_item'),
+    # delete item
+    path('delete/<int:id>/', views.delete_item, name='delete_item'),
 ]
